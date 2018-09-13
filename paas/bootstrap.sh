1ba4d6e2adc0566bcae10980bdad45d58aaad1b7
Sven Hettwer <sven.hettwer@consol.de>
2018-09-13 11:03:29 +0200

    Initial import

---
+++ paas/bootstrap.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+MINISHIFT_VM_DRIVER=virtualbox
+MINISHIFT_MEMORY=8GB
+MINISHIFT_DISK_SIZE=40GB
+
+OPENSHIFT_VERSION=3.10.0
+
+
+minishift profiles set jcon-containerized-test-automation
+minishift start \
+        --vm-driver=${MINISHIFT_VM_DRIVER} \
+        --memory=${MINISHIFT_MEMORY} \
+        --openshift-version ${OPENSHIFT_VERSION} \
+        --disk-size=${MINISHIFT_DISK_SIZE}
+oc login -u system:admin
+
+#Setup gogs repository
+oc new-project gogs
+oc new-app -f http://bit.ly/openshift-gogs-template --param=HOSTNAME=gogs.$(minishift ip).nip.io
+#First registered user is admin
+#Manual import of the project into gogs required https://github.com/svettwer/JCON-2018-Containerized-Test-Automation.git as 'todo-app'
+
+#Setup offline s2i build
+oc project openshift
+oc process -f $(git rev-parse --show-toplevel)/paas/yml/s2i_spring_offline_build_template.yml | oc apply -f -
+oc start-build spring-offline-s2i
